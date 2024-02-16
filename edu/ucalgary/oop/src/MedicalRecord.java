22966ddf9b18db8adb18fd8f9b71c2287e7f0e58
Afif2K <afifahmed.chaudhury@ucalgary.ca>
2024-02-16 14:48:37 -0700

    fucked

---
+++ edu/ucalgary/oop/src/MedicalRecord.java
@@ -0,0 +1,55 @@
+public class MedicalRecord {
+    private Location location;
+    private String treatmentDetails;
+    private String dateOfTreatment;
+    // Constructor
+    public MedicalRecord(Location location, String treatmentDetails, String dateOfTreatment) {
+        if (location == null || treatmentDetails == null || dateOfTreatment == null) {
+            throw new IllegalArgumentException("Arguments cannot be null");
+        }
+        this.location = location;
+        this.treatmentDetails = treatmentDetails;
+        this.dateOfTreatment = dateOfTreatment;
+    }
+
+    // Getters and Setters
+    public DisasterVictim getVictim() {
+        DisasterVictim[] occupants = location.getOccupants();
+        if (occupants.length > 0) {
+            return occupants[0]; // Assuming there's only one victim per medical record
+        } else {
+            return null; // Return null if no victim found
+        }
+    }
+
+    public void setVictim(DisasterVictim victim) {
+        // Not implementing this setter assuming victim is derived from location
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getTreatmentDetails() {
+        return treatmentDetails;
+    }
+
+    public void setTreatmentDetails(String treatmentDetails) {
+        this.treatmentDetails = treatmentDetails;
+    }
+
+    public String getDateOfTreatment() {
+        return dateOfTreatment;
+    }
+
+    public void setDateOfTreatment(String dateOfTreatment) {
+        if (dateOfTreatment == null) {
+            throw new IllegalArgumentException("Error, set date of treatment");
+        }
+        this.dateOfTreatment = dateOfTreatment;
+    }
+}
---
+++ edu/ucalgary/oop/src/ReliefService.java
@@ -0,0 +1,75 @@
+public class ReliefService {
+    private Inquirer inquirer;
+    private DisasterVictim missingPerson;
+    private String dateOfInquiry;
+    private String infoProvided;
+    private Location lastKnownLocation;
+
+    // Constructor
+    public ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry,
+                         String infoProvided, Location lastKnownLocation) {
+        this.inquirer = inquirer;
+        this.missingPerson = missingPerson;
+        this.dateOfInquiry = dateOfInquiry;
+        this.infoProvided = infoProvided;
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+    // Getters and Setters
+    public Inquirer getInquirer() {
+        return inquirer;
+    }
+
+    public void setInquirer(Inquirer inquirer) {
+        this.inquirer = inquirer;
+    }
+
+    public DisasterVictim getMissingPerson() {
+        return missingPerson;
+    }
+
+    public void setMissingPerson(DisasterVictim missingPerson) {
+        this.missingPerson = missingPerson;
+    }
+
+    public String getDateOfInquiry() {
+        return dateOfInquiry;
+    }
+
+    public void setDateOfInquiry(String dateOfInquiry) {
+        if (dateOfInquiry == null || dateOfInquiry.isEmpty()) {
+            throw new IllegalArgumentException("Date of inquiry cannot be null or empty");
+        }
+        this.dateOfInquiry = dateOfInquiry;
+    }
+
+    public String getInfoProvided() {
+        return infoProvided;
+    }
+
+    public void setInfoProvided(String infoProvided) {
+        this.infoProvided = infoProvided;
+    }
+
+    public Location getLastKnownLocation() {
+        return lastKnownLocation;
+    }
+
+    public void setLastKnownLocation(Location lastKnownLocation) {
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+    // Additional methods
+    public void printLogDetails() {
+        // Print log details
+        System.out.println(getLogDetails());
+    }
+    //will finish this part out later
+    public String getLogDetails() {
+        return "Inquirer: " + inquirer.getFirstName() + "\n" +
+                "Missing Person: " + missingPerson.getFirstName() + "\n" +
+                "Date of Inquiry: " + dateOfInquiry + "\n" +
+                "Info Provided: " + infoProvided + "\n" +
+                "Last Known Location: " + lastKnownLocation.getName() + ", " + lastKnownLocation.getAddress();
+    }
+}
